424aa8bfdbcf3c7e796f6f68fc0639ee0f588ef0
kuroneko1997 <113764840+qwerdfg45@users.noreply.github.com>
2026-07-20 13:08:01 +0800

    Add files via upload

---
+++ Forms/AssetTypeIconConverter.cs
@@ -0,0 +1,113 @@
+﻿using AssetsTools.NET.Extra;
+using Avalonia;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using HarfBuzzSharp;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UABEAvalonia
+{
+    public class AssetTypeIconConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is AssetClassID assetClass)
+            {
+                if ((int)assetClass < 0)
+                {
+                    return GetBitmap("UABEAvalonia/Assets/Icons/asset-mono-behaviour.png");
+                }
+
+                return assetClass switch
+                {
+                    AssetClassID.Animation => GetBitmap("UABEAvalonia/Assets/Icons/asset-animation.png"),
+                    AssetClassID.AnimationClip => GetBitmap("UABEAvalonia/Assets/Icons/asset-animation-clip.png"),
+                    AssetClassID.Animator => GetBitmap("UABEAvalonia/Assets/Icons/asset-animator.png"),
+                    AssetClassID.AnimatorController => GetBitmap("UABEAvalonia/Assets/Icons/asset-animator-controller.png"),
+                    AssetClassID.AnimatorOverrideController => GetBitmap("UABEAvalonia/Assets/Icons/asset-animator-override-controller.png"),
+                    AssetClassID.AudioClip => GetBitmap("UABEAvalonia/Assets/Icons/asset-audio-clip.png"),
+                    AssetClassID.AudioListener => GetBitmap("UABEAvalonia/Assets/Icons/asset-audio-listener.png"),
+                    AssetClassID.AudioMixer => GetBitmap("UABEAvalonia/Assets/Icons/asset-audio-mixer.png"),
+                    AssetClassID.AudioMixerGroup => GetBitmap("UABEAvalonia/Assets/Icons/asset-audio-mixer-group.png"),
+                    AssetClassID.AudioSource => GetBitmap("UABEAvalonia/Assets/Icons/asset-audio-source.png"),
+                    AssetClassID.Avatar => GetBitmap("UABEAvalonia/Assets/Icons/asset-avatar.png"),
+                    AssetClassID.BillboardAsset => GetBitmap("UABEAvalonia/Assets/Icons/asset-billboard.png"),
+                    AssetClassID.BillboardRenderer => GetBitmap("UABEAvalonia/Assets/Icons/asset-billboard-renderer.png"),
+                    AssetClassID.BoxCollider => GetBitmap("UABEAvalonia/Assets/Icons/asset-box-collider.png"),
+                    AssetClassID.Camera => GetBitmap("UABEAvalonia/Assets/Icons/asset-camera.png"),
+                    AssetClassID.Canvas => GetBitmap("UABEAvalonia/Assets/Icons/asset-canvas.png"),
+                    AssetClassID.CanvasGroup => GetBitmap("UABEAvalonia/Assets/Icons/asset-canvas-group.png"),
+                    AssetClassID.CanvasRenderer => GetBitmap("UABEAvalonia/Assets/Icons/asset-canvas-renderer.png"),
+                    AssetClassID.CapsuleCollider => GetBitmap("UABEAvalonia/Assets/Icons/asset-capsule-collider.png"),
+                    AssetClassID.CapsuleCollider2D => GetBitmap("UABEAvalonia/Assets/Icons/asset-capsule-collider.png"),
+                    AssetClassID.ComputeShader => GetBitmap("UABEAvalonia/Assets/Icons/asset-compute-shader.png"),
+                    AssetClassID.Cubemap => GetBitmap("UABEAvalonia/Assets/Icons/asset-cubemap.png"),
+                    AssetClassID.Flare => GetBitmap("UABEAvalonia/Assets/Icons/asset-flare.png"),
+                    AssetClassID.FlareLayer => GetBitmap("UABEAvalonia/Assets/Icons/asset-flare-layer.png"),
+                    AssetClassID.Font => GetBitmap("UABEAvalonia/Assets/Icons/asset-font.png"),
+                    AssetClassID.GameObject => GetBitmap("UABEAvalonia/Assets/Icons/asset-game-object.png"),
+                    AssetClassID.Light => GetBitmap("UABEAvalonia/Assets/Icons/asset-light.png"),
+                    AssetClassID.LightmapSettings => GetBitmap("UABEAvalonia/Assets/Icons/asset-lightmap-settings.png"),
+                    AssetClassID.LODGroup => GetBitmap("UABEAvalonia/Assets/Icons/asset-lod-group.png"),
+                    AssetClassID.Material => GetBitmap("UABEAvalonia/Assets/Icons/asset-material.png"),
+                    AssetClassID.Mesh => GetBitmap("UABEAvalonia/Assets/Icons/asset-mesh.png"),
+                    AssetClassID.MeshCollider => GetBitmap("UABEAvalonia/Assets/Icons/asset-mesh-collider.png"),
+                    AssetClassID.MeshFilter => GetBitmap("UABEAvalonia/Assets/Icons/asset-mesh-filter.png"),
+                    AssetClassID.MeshRenderer => GetBitmap("UABEAvalonia/Assets/Icons/asset-mesh-renderer.png"),
+                    AssetClassID.MonoBehaviour => GetBitmap("UABEAvalonia/Assets/Icons/asset-mono-behaviour.png"),
+                    AssetClassID.MonoScript => GetBitmap("UABEAvalonia/Assets/Icons/asset-mono-script.png"),
+                    AssetClassID.NavMeshSettings => GetBitmap("UABEAvalonia/Assets/Icons/asset-nav-mesh-settings.png"),
+                    AssetClassID.ParticleSystem => GetBitmap("UABEAvalonia/Assets/Icons/asset-particle-system.png"),
+                    AssetClassID.ParticleSystemRenderer => GetBitmap("UABEAvalonia/Assets/Icons/asset-particle-system-renderer.png"),
+                    AssetClassID.RectTransform => GetBitmap("UABEAvalonia/Assets/Icons/asset-rect-transform.png"),
+                    AssetClassID.ReflectionProbe => GetBitmap("UABEAvalonia/Assets/Icons/asset-reflection-probe.png"),
+                    AssetClassID.Rigidbody => GetBitmap("UABEAvalonia/Assets/Icons/asset-rigidbody.png"),
+                    AssetClassID.Shader => GetBitmap("UABEAvalonia/Assets/Icons/asset-shader.png"),
+                    AssetClassID.ShaderVariantCollection => GetBitmap("UABEAvalonia/Assets/Icons/asset-shader-collection.png"),
+                    AssetClassID.SkinnedMeshRenderer => GetBitmap("UABEAvalonia/Assets/Icons/asset-mesh-renderer.png"), // todo
+                    AssetClassID.Sprite => GetBitmap("UABEAvalonia/Assets/Icons/asset-sprite.png"),
+                    AssetClassID.SpriteRenderer => GetBitmap("UABEAvalonia/Assets/Icons/asset-sprite-renderer.png"),
+                    AssetClassID.Terrain => GetBitmap("UABEAvalonia/Assets/Icons/asset-terrain.png"),
+                    AssetClassID.TerrainCollider => GetBitmap("UABEAvalonia/Assets/Icons/asset-terrain-collider.png"),
+                    AssetClassID.TextAsset => GetBitmap("UABEAvalonia/Assets/Icons/asset-text-asset.png"),
+                    AssetClassID.Texture2D => GetBitmap("UABEAvalonia/Assets/Icons/asset-texture2d.png"),
+                    AssetClassID.Texture3D => GetBitmap("UABEAvalonia/Assets/Icons/asset-texture2d.png"),
+                    AssetClassID.Transform => GetBitmap("UABEAvalonia/Assets/Icons/asset-transform.png"),
+                    _ => GetBitmap("UABEAvalonia/Assets/Icons/asset-unknown.png"),
+                };
+            }
+
+            return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+        }
+
+
+        Dictionary<string, Bitmap> cache = new();
+
+        private Bitmap GetBitmap(string path)
+        {
+            Bitmap? bitmap;
+            if (cache.TryGetValue(path, out bitmap))
+            {
+                return bitmap;
+            }
+            else
+            {
+                bitmap = new Bitmap(AssetLoader.Open(new Uri($"avares://{path}")));
+                cache[path] = bitmap;
+                return bitmap;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Forms/AssetsFileInfo/AssetsFileInfoWindow.TypeTree.axaml.cs
@@ -0,0 +1,205 @@
+﻿using AssetsTools.NET;
+using AssetsTools.NET.Extra;
+using Avalonia.Controls;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace UABEAvalonia
+{
+    public partial class AssetsFileInfoWindow
+    {
+        private void SetupTypeTreePageEvents()
+        {
+            lstTypeTreeType.SelectionChanged += TypeTreeTypeList_SelectionChanged;
+            treeTypeTreeNode.SelectionChanged += TreeTypeTreeNode_SelectionChanged;
+        }
+
+        private void TreeTypeTreeNode_SelectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            if (treeTypeTreeNode.SelectedItem is TreeViewItem item)
+            {
+                if (item.Tag is TypeTreeNode node)
+                {
+                    FillTypeTreeNodeInfo(node);
+                }
+            }
+        }
+
+        private void TypeTreeTypeList_SelectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            if (lstTypeTreeType.SelectedItem is TypeTreeListItem item)
+            {
+                FillTypeTreeTypeInfo(item.type);
+            }
+        }
+
+        private void FillTypeTreeInfo()
+        {
+            AssetsFile afile = activeFile.file;
+            AssetsFileMetadata meta = afile.Metadata;
+
+            if (!meta.TypeTreeEnabled)
+            {
+                treeTypeTreeNode.ItemsSource = new List<string>()
+                {
+                    "There is no type tree data available."
+                };
+            }
+            else
+            {
+                treeTypeTreeNode.ItemsSource = new List<string>()
+                {
+                    "Select a type to show the type tree data."
+                };
+            }
+
+            List<TypeTreeListItem> typeListItems = new List<TypeTreeListItem>();
+            AddTypeTreeListItems(typeListItems, meta.TypeTreeTypes, false);
+            if (meta.RefTypes != null)
+            {
+                AddTypeTreeListItems(typeListItems, meta.RefTypes, true);
+            }
+
+            lstTypeTreeType.ItemsSource = typeListItems;
+        }
+
+        private void AddTypeTreeListItems(List<TypeTreeListItem> into, List<TypeTreeType> types, bool refType)
+        {
+            foreach (TypeTreeType type in types)
+            {
+                string typeName;
+                if (type.Nodes == null || type.Nodes.Count == 0)
+                {
+                    ClassDatabaseType dbType = cldb.FindAssetClassByID(type.TypeId);
+                    typeName = cldb.GetString(dbType.Name);
+                }
+                else
+                {
+                    TypeTreeNode baseField = type.Nodes[0];
+                    typeName = baseField.GetTypeString(type.StringBuffer);
+                }
+
+                string scriptIndexStr = string.Empty;
+                if (type.ScriptTypeIndex != 0xffff)
+                {
+                    scriptIndexStr = $"/{type.ScriptTypeIndex:d4}";
+                }
+
+                string refInfo = refType ? " REF" : string.Empty;
+                into.Add(new TypeTreeListItem($"{typeName} (0x{type.TypeId:x}{scriptIndexStr}){refInfo}", type));
+            }
+        }
+
+        private void FillTypeTreeTypeInfo(TypeTreeType type)
+        {
+            AssetsFile afile = activeFile.file;
+            AssetsFileMetadata meta = afile.Metadata;
+
+            if (type.Nodes == null || type.Nodes.Count == 0)
+            {
+                ClassDatabaseType cldt = cldb.FindAssetClassByID(type.TypeId);
+                boxTypeTreeType.Text = cldb.GetString(cldt.Name);
+            }
+            else
+            {
+                TypeTreeNode baseField = type.Nodes[0];
+                boxTypeTreeType.Text = baseField.GetTypeString(type.StringBuffer);
+            }
+
+            boxTypeTreeTypeId.Text = $"{type.TypeId} (0x{type.TypeId:x})";
+            if (type.ScriptTypeIndex != 0xffff)
+            {
+                string scriptName;
+                try
+                {
+                    AssetTypeReference? scriptInfo = AssetHelper.GetAssetsFileScriptInfo(am, activeFile, type.ScriptTypeIndex);
+                    scriptName = scriptInfo?.ClassName ?? "UNKNOWN";
+                }
+                catch
+                {
+                    scriptName = "UNKNOWN";
+                }
+
+                boxTypeTreeScriptId.Text = $"{type.ScriptTypeIndex} ({scriptName})";
+            }
+            else
+            {
+                boxTypeTreeScriptId.Text = string.Empty;
+            }
+
+            if (!type.TypeHash.IsZero())
+                boxTypeTreeHash.Text = type.TypeHash.ToString();
+            else
+                boxTypeTreeHash.Text = string.Empty;
+
+            if (!type.ScriptIdHash.IsZero())
+                boxTypeTreeMonoHash.Text = type.ScriptIdHash.ToString();
+            else
+                boxTypeTreeMonoHash.Text = string.Empty;
+
+            if (meta.TypeTreeEnabled)
+                FillTypeTreeNodeTree(type);
+        }
+
+        private void FillTypeTreeNodeInfo(TypeTreeNode node)
+        {
+            boxTypeTreeAligned.Text = (node.MetaFlags & 0x4000) != 0 ? "true" : "false";
+        }
+
+        private void FillTypeTreeNodeTree(TypeTreeType type)
+        {
+            var treeViewItems = new List<TreeViewItem>();
+            var treeNodeItemsStack = new List<ObservableCollection<TreeViewItem>>();
+
+            TypeTreeNode baseField = type.Nodes[0];
+            TreeViewItem rootNode = MakeTreeViewItem(TypeFieldToString(baseField, type), baseField);
+            treeViewItems.Add(rootNode);
+            treeNodeItemsStack.Add((ObservableCollection<TreeViewItem>)rootNode.ItemsSource!);
+
+            for (int i = 1; i < type.Nodes.Count; i++)
+            {
+                TypeTreeNode field = type.Nodes[i];
+                ObservableCollection<TreeViewItem> parentNodeItems = treeNodeItemsStack[field.Level - 1];
+                TreeViewItem node = MakeTreeViewItem(TypeFieldToString(field, type), field);
+
+                parentNodeItems?.Add(node);
+
+                node.Tag = field;
+                if (treeNodeItemsStack.Count > field.Level)
+                    treeNodeItemsStack[field.Level] = (ObservableCollection<TreeViewItem>)node.ItemsSource!;
+                else
+                    treeNodeItemsStack.Add((ObservableCollection<TreeViewItem>)node.ItemsSource!);
+            }
+
+            treeTypeTreeNode.ItemsSource = treeViewItems;
+        }
+
+        private TreeViewItem MakeTreeViewItem(string header, object tag)
+        {
+            return new TreeViewItem() { Header = header, Tag = tag, ItemsSource = new ObservableCollection<TreeViewItem>() };
+        }
+
+        private string TypeFieldToString(TypeTreeNode node, TypeTreeType type)
+        {
+            string stringTable = type.StringBuffer;
+            return $"{node.GetTypeString(stringTable)} {node.GetNameString(stringTable)}";
+        }
+
+        private class TypeTreeListItem
+        {
+            public string text;
+            public TypeTreeType type;
+
+            public TypeTreeListItem(string text, TypeTreeType type)
+            {
+                this.text = text;
+                this.type = type;
+            }
+
+            public override string ToString()
+            {
+                return text;
+            }
+        }
+    }
+}
